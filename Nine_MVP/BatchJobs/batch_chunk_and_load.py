3e1d966500130eddab050b533866c51c8552f331
tara pourhabibi <tara.pourhabibi@intelia.com.au>
2024-10-05 07:17:22 +0000

    adding batch job changes

--- Nine_MVP/BatchJobs/batch_chunk_and_load.py
+++ Nine_MVP/BatchJobs/batch_chunk_and_load.py
@@ -5,10 +5,9 @@ from langchain_google_community import BigQueryVectorStore,BigQueryLoader
 from datetime import datetime
 import os
 import logging
-
-
+import json
     
-def chunk_and_embedding(project_id: str= None, dataset: str= None, table: str= None, region: str =None,\
+def chunk_and_load(project_id: str= None, dataset: str= None, table: str= None, region: str =None,\
                         metadata_columns: list[str]=None, page_content_columns: list[str]= None, \
                         source_query_str: str= None, separators:  list[str]=None, chunk_size: int=None, \
                        chunk_overlap: int=0):
@@ -22,23 +21,21 @@ def chunk_and_embedding(project_id: str= None, dataset: str= None, table: str= N
         Returns:
              
         """
-    
+
 
      # Load the data
     loader = BigQueryLoader(
         source_query_str, metadata_columns=metadata_columns, page_content_columns=page_content_columns
     )
 
-    embedding = VertexAIEmbeddings(
-        model_name="textembedding-gecko@latest", project=project_id
-    )
-
-   
     documents = []
     documents.extend(loader.load())
     
     logging.info (f"Data Loaded from source - {source_query_str}")
-
+   
+    print('you are here')
+    print(chunk_size)
+    print(chunk_overlap)
 
     # Split the documents into chunks
     text_splitter = RecursiveCharacterTextSplitter(
@@ -57,6 +54,7 @@ def chunk_and_embedding(project_id: str= None, dataset: str= None, table: str= N
     # Add chunk number to metadata
     chunk_idx=0
     prev=doc_splits[0].metadata["id"]
+    rows_to_insert=[]
     for idx, split in enumerate(doc_splits):
         split.metadata["process_time"]=now
         if prev==split.metadata["id"]:
@@ -66,23 +64,36 @@ def chunk_and_embedding(project_id: str= None, dataset: str= None, table: str= N
             split.metadata["chunk"] = chunk_idx
             prev=split.metadata["id"]
         chunk_idx +=1
-        
+
+        rows_to_insert.append(
+                           {"id": split.metadata["id"], 
+                               "process_time":split.metadata["process_time"].isoformat(),
+                               "content":  split.page_content,
+                               "chunk": split.metadata["chunk"]
+                              }
+                                     )
     logging.info (f"Metadata added to chunks")
 
+    from google.cloud import bigquery
+
+    client = bigquery.Client()
+    table_id = f"{project_id}.{dataset}.{table}"
+
+ 
+    errors = client.insert_rows_json(table_id, rows_to_insert)
+    if errors == []:
+        print("New rows have been added.")
+    else:
+        print(f"Encountered errors: {errors}")
     
-    bq_store = BigQueryVectorStore(
-        project_id=project_id,
-        dataset_name=dataset,
-        table_name=table,
-        location=region,
-        embedding=embedding,
-    ) 
-    logging.info (f"Bigquery store info is set -  ProjectID {project_id}, Region {region}, Dataset {dataset}, Table {table}")
+    
+    
+#     logging.info (f"Bigquery store info is set -  ProjectID {project_id}, Region {region}, Dataset {dataset}, Table {table}")
 
 
-    _=bq_store.add_documents(doc_splits)
+#     _=bq_store.add_documents(doc_splits)
     
-    logging.info (f"Chunks and embeddings added to the store")
+#     logging.info (f"Chunks and embeddings added to the store")
 
     return 'done'
 
@@ -95,26 +106,29 @@ if __name__ == "__main__":
     page_content_columns= str(os.environ.get("PAGE_CONTENT_COLUMNS")).split(',') 
     source_query_str= os.environ.get("SOURCE_QUERY_STR") 
     separators= None if str(os.environ.get("SEPARATORS"))=="" else str(os.environ.get("SEPARATORS")).split(',') 
-    chunk_size= os.environ.get("CHUNK_SIZE")
-    chunk_overlap= 0 if os.environ.get("CHUNK_OVERLAP")=="" else os.environ.get("CHUNK_OVERLAP")
+    chunk_size= 1000 if str(os.environ.get("CHUNK_SIZE"))=="None" else int(str(os.environ.get("CHUNK_SIZE")))  
+    chunk_overlap= 0 if str(os.environ.get("CHUNK_OVERLAP"))=="None" else int(str(os.environ.get("CHUNK_OVERLAP")))
  
+ 
+    print(chunk_size)
+    print(chunk_overlap)
 
-    project_id= 'nine-quality-test' 
-    dataset= 'my_langchain_dataset'
-    table= 'doc_and_vectors'
-    region= 'us-central1'
-    metadata_columns= "id".split(",")
-    page_content_columns= "content,media_type,test_metadata".split(',') 
-    source_query_str= """
-    SELECT id,media_type,content,test_metadata 
-    FROM `nine-quality-test.Nine_Quality_Test.content_embeddings` ;
-    """
-    separators= "\n\n"
-    chunk_size= 25
-    chunk_overlap= 5
+    # project_id= 'nine-quality-test' 
+    # dataset= 'my_langchain_dataset'
+    # table= 'doc_and_vectors'
+    # region= 'us-central1'
+    # metadata_columns= "id".split(",")
+    # page_content_columns= "content,media_type,test_metadata".split(',') 
+    # source_query_str= """
+    # SELECT id,media_type,content,test_metadata 
+    # FROM `nine-quality-test.Nine_Quality_Test.content_embeddings` ;
+    # """
+    # separators= "\n\n"
+    # chunk_size= 25
+    # chunk_overlap= 5
     
     
-    message=chunk_and_embedding(project_id=project_id, dataset=dataset, table=table, region=region,\
+    message=chunk_and_load(project_id=project_id, dataset=dataset, table=table, region=region,\
                         metadata_columns= metadata_columns, page_content_columns=page_content_columns, \
                         source_query_str=source_query_str, separators=separators, chunk_size=chunk_size, \
                        chunk_overlap=chunk_overlap)
